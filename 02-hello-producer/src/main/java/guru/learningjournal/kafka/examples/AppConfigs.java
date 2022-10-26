124cf2c5889c95c2fccfb1bf60e022d98048d1c6
Prateek <54174687+javaHelper@users.noreply.github.com>
2022-10-26 20:48:15 +0530

    02 - Hello Producer

---
+++ 02-hello-producer/src/main/java/guru/learningjournal/kafka/examples/AppConfigs.java
@@ -0,0 +1,8 @@
+package guru.learningjournal.kafka.examples;
+
+public class AppConfigs {
+    public static final String applicationID = "HelloProducer";
+    public static final String bootstrapServers = "localhost:9092";
+    public static final String topicName = "hello-producer-topic";
+    public static final int numEvents = 1000000;
+}
