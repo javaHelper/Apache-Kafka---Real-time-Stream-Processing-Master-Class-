c9fab9821e7feb8aa30da0356c4fd5f9c95258ac
Prateek <54174687+javaHelper@users.noreply.github.com>
2022-10-28 17:48:43 +0530

    Pos Simulator avro

---
+++ 15-pos-simulator-avro/scripts/create-topic.sh
@@ -0,0 +1,2 @@
+#$KAFKA_HOME/bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 3 --partitions 2 --topic pos
+kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 2 --topic pos
