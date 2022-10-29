d81236bdf16b1e123c49926c09501d10b2d8d502
Prateek <54174687+javaHelper@users.noreply.github.com>
2022-10-29 09:12:03 +0530

    Partioner

--- 08-partition-producer/src/main/java/guru/learningjournal/kafka/examples/OddEvenPartitioner.java
+++ 08-partition-producer/src/main/java/guru/learningjournal/kafka/examples/OddEvenPartitioner.java
@@ -11,7 +11,7 @@ public class OddEvenPartitioner implements Partitioner {
     @Override
     public int partition(String topic, Object key, byte[] keyBytes, Object value, byte[] valueBytes, Cluster cluster) {
         if ((keyBytes == null) || (!(key instanceof Integer)))
-            throw new InvalidRecordException("Topic Key must have a valid Integer value.");
+            throw new InvalidRecordException("## Topic Key must have a valid Integer value.");
 
         if (cluster.partitionsForTopic(topic).size() != 2)
             throw new InvalidTopicException("Topic must have exactly two partitions");
