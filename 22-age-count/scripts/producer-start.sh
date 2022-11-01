8b01fc764f8a275ba39b901b881fdcb72e5275c9
Prateek <54174687+javaHelper@users.noreply.github.com>
2022-11-01 16:24:17 +0530

    Age Count

--- 22-age-count/scripts/producer-start.sh
+++ 22-age-count/scripts/producer-start.sh
@@ -1,1 +1,1 @@
-$KAFKA_HOME/bin/kafka-console-producer.sh --broker-list localhost:9092 --topic person-age  --property parse.key=true --property key.separator=":"
+kafka-console-producer.sh --broker-list localhost:9092 --topic person-age  --property parse.key=true --property key.separator=":"
--- 22-age-count/scripts/topic-create.sh
+++ 22-age-count/scripts/topic-create.sh
@@ -1,1 +1,1 @@
-$KAFKA_HOME/bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic person-age
+kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic person-age
